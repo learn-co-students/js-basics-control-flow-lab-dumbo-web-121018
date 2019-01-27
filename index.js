4723348e40be757d3ca56935613e6f1ff0a2c048
tarasabella <tarasabella@gmail.com>
2019-01-27 01:42:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(someValue) {
+
+  let result;
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  }
+  else if (someValue > 2000) {
+    if (someValue > 2500) {
+      result = "No can do.";
+    }
+    else {
+      result = "I will gladly take your thirty bucks.";
+    }
+  }
+
+    return result;
+}
+
+function ternaryCheckCity(destination) {
+
+let response;
+  destination === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+return response;
+
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch(tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default: response = "Bye.";
+  }
+  return response;
+}
