c4d5fcf019f7e2259d8770fda27559a7b48182b3
Bennimaru <bennywhyu@gamil.com>
2019-01-27 22:27:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num){
+  let greeting
+  if (num < 400){
+    greeting = "This one is on me!"
+  }
+  else if (num>2000 && num <2500){
+    greeting = "I will gladly take your thirty bucks."
+  }
+  else{
+    greeting = "No can do."
+  }
+  return greeting
+}
+
+function ternaryCheckCity(city){
+return city === "NYC"? "Ok, sounds good.": "No go.";
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+        return "Thank you so much.";
+    case 'not as generous':
+        return "Thank you."
+    default:
+        return "Bye."
+    }
+}
